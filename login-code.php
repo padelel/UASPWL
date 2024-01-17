562db40e95e782ad384e3eaf43fcd7ea050bb511
Fadel <mfiva213@gmail.com>
2024-01-18 01:39:20 +0700

    Nambah Captcha

--- login-code.php
+++ login-code.php
@@ -1,11 +1,22 @@
 <?php
-
 require 'config/function.php';
 
-if (isset($_POST['loginBtn'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Verifikasi reCAPTCHA
+    $recaptchaSecretKey = '6LctJFQpAAAAADhrp6bbDo24l670K7YZ0eLyZ1Zf';
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+    $recaptchaUrl = "https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaSecretKey&response=$recaptchaResponse";
+    $recaptchaData = json_decode(file_get_contents($recaptchaUrl));
+
+    if (!$recaptchaData->success) {
+        setAlert('danger', 'reCAPTCHA verification failed. Please try again.');
+        header('Location: login.php');
+        exit;
+    }
+
+    // Proses login jika reCAPTCHA valid
     $name = validate($_POST['name']);
     $password = validate($_POST['password']);
-
 
     $query = "SELECT * FROM admins WHERE name ='$name' AND password ='$password'";
     $result = mysqli_query($conn, $query);
@@ -13,9 +24,14 @@ if (isset($_POST['loginBtn'])) {
     $count = mysqli_num_rows($result);
 
     if ($count == 1) {
-        redirect('admin/index.php', 'login succcess');
         $_SESSION['name'] = $name;
+        redirect('admin/index.php', 'Login success');
     } else {
         redirect('login.php', 'Invalid username or password');
     }
+} else {
+    // Redirect jika mencoba mengakses secara langsung
+    header('Location: login.php');
+    exit;
 }
+?>
--- login.php
+++ login.php
@@ -8,7 +8,7 @@ require 'config/function.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FOOD WEBSITE</title>
+    <title>GAMESTORE WEBSITE</title>
     <link rel="stylesheet" href="login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
@@ -27,12 +27,13 @@ require 'config/function.php';
                                 <div class="mb-3">
                                     <label>Username</label>
                                     <input type="text" name="name" class="form-control" required>
-                                </div>
-                                <div class="mb-3">
+                                    </div>
+                                    <div class="mb-3">
                                     <label>Password</label>
                                     <input type="password" name="password" class="form-control" required>
-                                </div>
-                                <div class="my-3">
+                                    </div>
+                                    <div class="g-recaptcha" data-sitekey="6LctJFQpAAAAAM45ff8NbdzzbmklywaCKo-vfDw1"></div>
+                                    <div class="my-3">
                                     <button type="submit" name="loginBtn" class="btn btn-success w-100 mt-2">Sign In</button>
                                 </div>
                             </form>
@@ -43,6 +44,7 @@ require 'config/function.php';
         </div>
     </div>
 
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
